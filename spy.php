2b45f0a49d9a87e09329f2012595c8ab6b90e63e
Sergi Torres <sergitorres17@gmail.com>
2015-11-03 16:35:59 +0100

    finished statistics and deployed to hostinger

--- spy.php
+++ spy.php
@@ -1,7 +1,7 @@
 <?php
 
 $mysqli = new mysqli('mysql.hostinger.es', 'u251939249_sergi', 'usersergi', 'u251939249_sergi');
-
+//$mysqli = new mysqli('localhost', 'root', '', 'users', 33060);
 
  if(!$mysqli || $mysqli->errno){
  	die('Error connectig to database');
@@ -15,35 +15,9 @@ $query = "Insert into visitas(origen,destino) values('$o','$d');";
 $result = $mysqli-> query($query);
 
 
-if($d == "DiarioIbiza"){
-header("Location: http://www.diariodeibiza.es/");  
+header("Location: http://$d");  
 die('redirected');
-}
 
-else if($d == "ElTiempoIbiza"){
-header("Location: http://www.eltiempo.es/ibiza.html");  
-die('redirected');
-}
-
-else if($d == "Facebook"){
-header("Location: https://www.facebook.com/");  
-die('redirected');
-}
-
-else if($d == "SitesSaColomina"){
-header("Location: https://sites.google.com/site/sacolominadaw1516/");  
-die('redirected');
-}
-
-else if($d == "Hostinger"){
-header("Location: http://www.hostinger.es/");  
-die('redirected');
-}
-
-else if($d == "GitHub"){
-header("Location: https://github.com/");  
-die('redirected');
-}
 
 
 ?>
